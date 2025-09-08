f756843dbe1a3b29b9e71ad1c26cb8f0e9d45785
KIT-kang <rkdtmdal94@kumoh.ac.kr>
2025-09-08 15:04:58 +0900

    update test

--- hellow_world.py
+++ hellow_world.py
@@ -1,5 +1,8 @@
 def print_hellow():
     print("Hellow world!!")
 
+def print_KIT():
+    print("Kunoh National Institude of Technology.")
+
 if __name__ == '__main__':
-    print_hellow()
+    print_KIT()
