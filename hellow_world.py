e2577c8417fae515c3fde48895ab34375bfc8ef1
KIT-kang <rkdtmdal94@kumoh.ac.kr>
2025-09-08 15:12:48 +0900

    second commit

--- hellow_world.py
+++ hellow_world.py
@@ -1,8 +1,10 @@
-def print_hellow():
-    print("Hellow world!!")
+def add(x, y):
+    a = x + y
+    return a
 
 def print_KIT():
-    print("Kunoh National Institude of Technology.")
+    print("KIT")
 
 if __name__ == '__main__':
     print_KIT()
+    print(add(1, 2))
